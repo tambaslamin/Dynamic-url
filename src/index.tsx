bd992bf29d2aac2c5a7effa8d636f4a6e965266e
Nick Russell <nicholas.russell@accenture.com>
2024-07-23 16:37:11 -0400

    Fix NewApp naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './newApp';
+import App from './NewApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
