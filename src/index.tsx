b6bf8707e31bf2af16bb396992ef681b1c8705a4
Nick Russell <nicholas.russell@accenture.com>
2024-07-23 16:46:03 -0400

    Changed import name/filename to "RefactoredApp"

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './NewApp';
+import App from './RefactoredApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
